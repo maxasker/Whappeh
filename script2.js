d50475a6b9256e14ea36494a2c7b99d307fcb1a5
maxasker <maxasker@hotmail.com>
2017-03-14 13:38:54 +0100

    ändrat lite

--- script2.js
+++ script2.js
@@ -1,16 +1,17 @@
-$.ajax({
-    url: "https://content.guardianapis.com/search?q=trump&api-key=f6a07e0d-f72f-47f9-ae61-a0d0446cef14",
+$("#search").on("click", function(){
+    var searchterm = $("#searchterm").val();
+	guardian(searchterm);
+});
+
+
+function guardian(searchterm){
+    $.ajax({
+    url: "https://content.guardianapis.com/search?q="+searchterm+"&show-fields=thumbnail&api-key=f6a07e0d-f72f-47f9-ae61-a0d0446cef14",
     dataType: "JSONP"
 }).done(function(data){
     console.log(data);
-    loopdatasemen(data);
 }).fail(function(data){
     console.log("something went wrong");
 });
 
-function loopdatasemen(data){
-    for (var i = 0; i < data["response"]["results"].length; i++){
-        $('#title').text(data["response"]["results"][i]["webTitle"]);
-    }
 }
-
